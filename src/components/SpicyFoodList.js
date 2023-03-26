4e4b963d82166ecfc804ad05748ec06d834b9b6f
san <sansamaa0@gmail.com>
2023-03-27 01:23:48 +0300

    reading

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -4,9 +4,15 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
+  // function handleAddFood() {
+  //   const newFood = getNewRandomSpicyFood();
+  //   console.log(newFood);
+  // }
+
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
   }
 
   const foodList = foods.map((food) => (
